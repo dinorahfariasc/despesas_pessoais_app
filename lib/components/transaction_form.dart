06b95e771e1ba35d9b6552a1e2cb2cdc61b0ee44
Dinorah <dinorahfarias@outlook.com>
2022-06-07 23:38:56 -0300

    commit

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 class TransactionForm extends StatefulWidget {
   TransactionForm(this.onSubmit);
@@ -11,14 +12,14 @@ class TransactionForm extends StatefulWidget {
 
 class _TransactionFormState extends State<TransactionForm> {
   // contructor
-  final titleController = TextEditingController();
-
-  final valueController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _valueController = TextEditingController();
+  DateTime? _selectedDate;
 
   _submitForm() {
     // funcao para submeter o formulario caso os campos estejam ok
-    final title = titleController.text;
-    final value = double.tryParse(valueController.text) ??
+    final title = _titleController.text;
+    final value = double.tryParse(_valueController.text) ??
         0.0; // se o valor der errado ele usa o 0 como padrao
 
     if (title.isEmpty || value <= 0) {
@@ -27,6 +28,23 @@ class _TransactionFormState extends State<TransactionForm> {
 
     widget.onSubmit(title,
         value); // momento que pega as informacoes do usuario e passa para a funcao
+  }
+
+  _showDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2019),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
   }
 
   @override
@@ -39,7 +57,7 @@ class _TransactionFormState extends State<TransactionForm> {
             onSubmitted: (_) {
               _submitForm();
             },
-            controller: titleController,
+            controller: _titleController,
             decoration: InputDecoration(labelText: 'Título'),
             inputFormatters: [LengthLimitingTextInputFormatter(35)],
           ),
@@ -48,7 +66,7 @@ class _TransactionFormState extends State<TransactionForm> {
               // oq fazer quando aperta no botao ok do teclado
               _submitForm();
             },
-            controller: valueController,
+            controller: _valueController,
             decoration: InputDecoration(labelText: 'Valor (R\$)'),
             keyboardType: TextInputType.numberWithOptions(
                 decimal: true), // trocar o teclado para so digitar numeros
@@ -60,13 +78,19 @@ class _TransactionFormState extends State<TransactionForm> {
             height: 70,
             child: Row(
               children: <Widget>[
-                Text('Nenhuma data selecionada'),
+                Expanded(
+                  child: Text(
+                    _selectedDate == null
+                        ? 'Nenhuma data selecionada!'
+                        : 'Data Selecionada: ${DateFormat('dd/MM/y').format(_selectedDate!)}',
+                  ),
+                ),
                 TextButton(
-                  onPressed: () {},
                   child: Text(
                     'Selecionar data',
                     style: TextStyle(fontWeight: FontWeight.bold),
                   ),
+                  onPressed: _showDatePicker,
                 ),
               ],
             ),
